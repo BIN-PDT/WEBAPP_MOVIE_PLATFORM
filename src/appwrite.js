011034b64b126e823f94d2a9f32f5112d41b9e8e
BIN BIN <thangpd300502@gmail.com>
2025-05-03 15:19:15 +0700

    STORE SEARCH COUNT

---
+++ src/appwrite.js
@@ -0,0 +1,43 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+	.setEndpoint("https://fra.cloud.appwrite.io/v1")
+	.setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (movie) => {
+	try {
+		const result = await database.listDocuments(
+			DATABASE_ID,
+			COLLECTION_ID,
+			[Query.equal("movie_id", movie.id)]
+		);
+
+		if (result.documents.length > 0) {
+			const document = result.documents[0];
+			await database.updateDocument(
+				DATABASE_ID,
+				COLLECTION_ID,
+				document.$id,
+				{ count: document.count + 1 }
+			);
+		} else {
+			await database.createDocument(
+				DATABASE_ID,
+				COLLECTION_ID,
+				ID.unique(),
+				{
+					movie_id: movie.id,
+					poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+				}
+			);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
